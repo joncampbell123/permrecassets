9ee96b62e2d7ed59feac6daf1f0e6a2e98696ebc
Jonathan Campbell <jonathan@castus.tv>
2019-08-18 18:24:24 -0700

    more

--- textsrc.cpp
+++ textsrc.cpp
@@ -7,160 +7,163 @@
 #include <unistd.h>
 #include <fcntl.h>
 
+#include "textsrc.hpp"
+
 #ifndef O_BINARY
 #define O_BINARY (0)
 #endif
 
 using namespace std;
 
-class TextSourceBase {
-public:
-    TextSourceBase() {
-    }
-    virtual ~TextSourceBase() {
-    }
-    virtual bool is_open(void) const {
-        return false;
-    }
-    virtual void close(void) {
-    }
-    virtual bool open(void) {
-        return false;
-    }
-    virtual int getc_direct(void) {
-        return -1;
-    }
-    virtual int getc_nl(void) {
-        /* for use in reading without considering newline formatting */
-        int c = getc();
-        if (c < 0) return -1;
+TextSourceBase::TextSourceBase() {
+}
 
-        /* 0x0A             Unix    \n          LF
-         * 0x0D 0x0A        DOS     \r\n        CR LF
-         * 0x0D             Mac     \r          CR
-         *
-         * For simplicity we do NOT support oddball constructions like 0x0D 0x0D 0x0A! */
-        if (c == 0x0D) {
-            c = getc();
-            if (c != 0x0A) {
-                if (!ungetc(c)) return -1;
-            }
+TextSourceBase::~TextSourceBase() {
+}
 
-            return '\n';
-        }
-        else if (c == 0x0A) {
-            return '\n';
+bool TextSourceBase::is_open(void) const {
+    return false;
+}
+
+void TextSourceBase::close(void) {
+}
+
+bool TextSourceBase::open(void) {
+    return false;
+}
+
+int TextSourceBase::getc_direct(void) {
+    return -1;
+}
+
+int TextSourceBase::getc_nl(void) {
+    /* for use in reading without considering newline formatting */
+    int c = getc();
+    if (c < 0) return -1;
+
+    /* 0x0A             Unix    \n          LF
+     * 0x0D 0x0A        DOS     \r\n        CR LF
+     * 0x0D             Mac     \r          CR
+     *
+     * For simplicity we do NOT support oddball constructions like 0x0D 0x0D 0x0A! */
+    if (c == 0x0D) {
+        c = getc();
+        if (c != 0x0A) {
+            if (!ungetc(c)) return -1;
         }
 
-        return c;
+        return '\n';
     }
-    virtual int getc(void) {
-        if (_unget) {
-            _unget = false;
-            return _unget_c;
+    else if (c == 0x0A) {
+        return '\n';
+    }
+
+    return c;
+}
+
+int TextSourceBase::getc(void) {
+    if (_unget) {
+        _unget = false;
+        return _unget_c;
+    }
+
+    return getc_direct();
+}
+
+bool TextSourceBase::ungetc(int c) {
+    if (_unget) return false;
+    _unget_c = c;
+    _unget = true;
+    return true;
+}
+
+bool TextSourceBase::eof(void) const {
+    return _eof;
+}
+
+bool TextSourceBase::error(void) const {
+    return _error;
+}
+
+TextSource::TextSource() : TextSourceBase() {
+}
+
+TextSource::TextSource(const string &_path) : TextSourceBase(), path(_path) {
+    open();
+}
+
+TextSource::~TextSource() {
+    close();
+}
+
+bool TextSource::is_open(void) const {
+    return (fd >= 0);
+}
+
+void TextSource::close(void) {
+    if (fd >= 0) {
+        ::close(fd);
+        fd = -1;
+    }
+}
+
+bool TextSource::open(void) {
+    if (!is_open()) {
+        if (path.empty()) return false;
+        fd = ::open(path.c_str(),O_RDONLY|O_BINARY);
+        if (fd < 0) return false;
+    }
+
+    return true;
+}
+
+int TextSource::getc_direct(void) {
+    if (is_open() && !error() && !eof()) {
+        char c;
+        int rd = read(fd,&c,1);
+        if (rd < 0) {
+            _error = true;
+            return rd;
         }
-
-        return getc_direct();
-    }
-    virtual bool ungetc(int c) {
-        if (_unget) return false;
-        _unget_c = c;
-        _unget = true;
-        return true;
-    }
-    virtual bool eof(void) const {
-        return _eof;
-    }
-    virtual bool error(void) const {
-        return _error;
-    }
-protected:
-    bool            _eof = false;
-    bool            _error = false;
-private:
-    bool            _unget = false;
-    int             _unget_c = -1;
-};
-
-class TextSource : public TextSourceBase {
-public:
-    TextSource() : TextSourceBase() {
-    }
-    TextSource(const string &_path) : TextSourceBase(), path(_path) {
-        open();
-    }
-    virtual ~TextSource() {
-        close();
-    }
-    virtual bool is_open(void) const {
-        return (fd >= 0);
-    }
-    virtual void close(void) {
-        if (fd >= 0) {
-            ::close(fd);
-            fd = -1;
+        else if (rd == 0) {
+            _eof = true;
+            return -1;
         }
+        return (int)((unsigned char)c);
     }
-    virtual bool open(void) {
-        if (!is_open()) {
-            if (path.empty()) return false;
-            fd = ::open(path.c_str(),O_RDONLY|O_BINARY);
-            if (fd < 0) return false;
+
+    return -1;
+}
+
+TextSourceStdin::TextSourceStdin() : TextSourceBase() {
+}
+
+TextSourceStdin::~TextSourceStdin() {
+}
+
+bool TextSourceStdin::is_open(void) const {
+    return true;
+}
+
+bool TextSourceStdin::open(void) {
+    return true;
+}
+
+int TextSourceStdin::getc_direct(void) {
+    if (is_open()) {
+        char c;
+        int rd = read(0/*stdin*/,&c,1);
+        if (rd < 0) {
+            _error = true;
+            return rd;
         }
-
-        return true;
-    }
-    virtual int getc_direct(void) {
-        if (is_open() && !error() && !eof()) {
-            char c;
-            int rd = read(fd,&c,1);
-            if (rd < 0) {
-                _error = true;
-                return rd;
-            }
-            else if (rd == 0) {
-                _eof = true;
-                return -1;
-            }
-            return (int)((unsigned char)c);
+        else if (rd == 0) {
+            _eof = true;
+            return -1;
         }
+        return (int)((unsigned char)c);
+    }
 
-        return -1;
-    }
-private:
-public:
-    int             fd = -1;
-    string          path;
-};
-
-class TextSourceStdin : public TextSourceBase {
-public:
-    TextSourceStdin() : TextSourceBase() {
-    }
-    virtual ~TextSourceStdin() {
-    }
-    virtual bool is_open(void) const {
-        return true;
-    }
-    virtual bool open(void) {
-        return true;
-    }
-    virtual int getc_direct(void) {
-        if (is_open()) {
-            char c;
-            int rd = read(0/*stdin*/,&c,1);
-            if (rd < 0) {
-                _error = true;
-                return rd;
-            }
-            else if (rd == 0) {
-                _eof = true;
-                return -1;
-            }
-            return (int)((unsigned char)c);
-        }
-
-        return -1;
-    }
-};
+    return -1;
+}
 
---
+++ textsrc.hpp
@@ -0,0 +1,51 @@
+
+#include <cstdio>
+#include <iostream>
+#include <cstring>
+#include <string>
+
+class TextSourceBase {
+public:
+    TextSourceBase();
+    virtual ~TextSourceBase();
+    virtual bool is_open(void) const;
+    virtual void close(void);
+    virtual bool open(void);
+    virtual int getc_direct(void);
+    virtual int getc_nl(void);
+    virtual int getc(void);
+    virtual bool ungetc(int c);
+    virtual bool eof(void) const;
+    virtual bool error(void) const;
+protected:
+    bool            _eof = false;
+    bool            _error = false;
+private:
+    bool            _unget = false;
+    int             _unget_c = -1;
+};
+
+class TextSource : public TextSourceBase {
+public:
+    TextSource();
+    TextSource(const std::string &_path);
+    virtual ~TextSource();
+    virtual bool is_open(void) const;
+    virtual void close(void);
+    virtual bool open(void);
+    virtual int getc_direct(void);
+private:
+public:
+    int             fd = -1;
+    std::string     path;
+};
+
+class TextSourceStdin : public TextSourceBase {
+public:
+    TextSourceStdin();
+    virtual ~TextSourceStdin();
+    virtual bool is_open(void) const;
+    virtual bool open(void);
+    virtual int getc_direct(void);
+};
+
