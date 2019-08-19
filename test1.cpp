4c2c0aecd2f05efc0ed56c5848a6b8cc57bad158
Jonathan Campbell <jonathan@castus.tv>
2019-08-18 17:22:48 -0700

    more

--- test1.cpp
+++ test1.cpp
@@ -43,6 +43,11 @@ int main(int argc,char **argv) {
     if (!parse_argv(argc,argv))
         return 1;
 
+    if (files.empty()) {
+        fprintf(stderr,"Please specify files to parse\n");
+        return 1;
+    }
+
     return 0;
 }
 
