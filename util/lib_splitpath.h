ae4862ec538b4e6529411f7f44d52566d9dc7e8b
Jonathan Campbell <jonathan@castus.tv>
2020-04-24 13:41:16 -0700

    more

--- util/lib_splitpath.h
+++ util/lib_splitpath.h
@@ -1,6 +1,10 @@
 
+#ifndef UTIL_LIB_SPLITPATH_H
+#define UTIL_LIB_SPLITPATH_H
+
 #include <string>
 #include <vector>
 
 void prl_path_split(std::vector<std::string> &spath,const std::string &path);
 
+#endif /*UTIL_LIB_SPLITPATH_H*/
