94cca72619c8b01b098b7ee4087ce8dfd26ee7fb
Jonathan Campbell <jonathan@castus.tv>
2020-04-25 00:25:42 -0700

    more

--- util/lib_prl_nodes.cpp
+++ util/lib_prl_nodes.cpp
@@ -1,5 +1,125 @@
 
+#include <assert.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <stdio.h>
+
+#include "lib_procmount.h"
+#include "lib_path_rel_label.h"
+#include "lib_prluuid.h"
+#include "lib_splitpath.h"
+#include "lib_prl_blob.h"
 #include "lib_prl_nodes.h"
 
+#include <sqlite3.h>
+
+using namespace std;
+
 const prluuid prl_zero_node = { 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0, 0,0,0,0 };
+
+static sqlite3* prl_node_db_sqlite = NULL;
+
+bool prl_node_db_open(void) {
+    if (prl_node_db_sqlite == NULL) {
+        if (sqlite3_open_v2("pra-fs-scan.db",&prl_node_db_sqlite,SQLITE_OPEN_READWRITE|SQLITE_OPEN_FULLMUTEX/*|SQLITE_OPEN_NOFOLLOW*/,NULL) != SQLITE_OK)
+            return false;
+
+        if (sqlite3_exec(prl_node_db_sqlite,"PRAGMA synchronous = 0;",NULL,NULL,NULL) != SQLITE_OK)
+            fprintf(stderr,"PRAGMA synchronous failed\n");
+    }
+
+    return true;
+}
+
+void prl_node_db_close(void) {
+    if (prl_node_db_sqlite != NULL) {
+        sqlite3_close_v2(prl_node_db_sqlite);
+        prl_node_db_sqlite = NULL;
+    }
+}
+
+/* in: ent.name, ent.parent_node
+ * out: ent.* */
+bool prl_node_db_add_fsentbyname(prl_node_entry &ent) {
+    sqlite3_stmt* stmt = NULL;
+    const char* pztail = NULL;
+    int results,sr;
+
+    /* add node with parent_node == parent_node, name = name
+     * If already exists, return without changing. */
+    /*                                                1                                                               */
+    /*                                                                             1                   2            3 */
+    if (sqlite3_prepare_v2(prl_node_db_sqlite,"SELECT node_id FROM nodes WHERE real_name = ? AND parent_node = ? AND type = ? LIMIT 1;",-1,&stmt,&pztail) != SQLITE_OK) {
+        fprintf(stderr,"db_add_archive statement prepare failed\n");
+        return false;
+    }
+    results = 0;
+    sqlite3_bind_blob(stmt,1,&ent.real_name[0],ent.real_name.size(),NULL);/*name*/
+    sqlite3_bind_blob(stmt,2,ent.parent_node.uuid,sizeof(ent.parent_node.uuid),NULL);/*parent_node*/
+    sqlite3_bind_int(stmt,3,ent.type);/*type*/
+    do {
+        sr = sqlite3_step(stmt);
+        if (sr == SQLITE_BUSY) continue;
+        else if (sr == SQLITE_DONE) break;
+        else if (sr == SQLITE_ROW) {
+            if (results == 0) {
+                int blobsz = sqlite3_column_bytes(stmt,0);
+                if (blobsz == sizeof(ent.node_id.uuid)) {
+                    const void *b = sqlite3_column_blob(stmt,0);
+                    if (b != NULL) memcpy(ent.node_id.uuid,b,sizeof(ent.node_id.uuid));
+                }
+            }
+            results++;
+        }
+        else {
+            fprintf(stderr,"SQLITE statement error\n");
+            break;
+        }
+    } while(1);
+    sqlite3_finalize(stmt);
+
+    if (results > 0) {
+        /* already there, nothing to do. The code updated node_id */
+        return true;
+    }
+
+    prluuidgen(ent.node_id);
+
+    /*                                                            1       2           3    4         5            6    7    8     9             1 2 3 4 5 6 7 8 9 */
+    if (sqlite3_prepare_v2(prl_node_db_sqlite,"INSERT INTO nodes (node_id,parent_node,name,real_name,name_charset,size,type,mtime,inode) VALUES(?,?,?,?,?,?,?,?,?);",-1,&stmt,&pztail) != SQLITE_OK) {
+        fprintf(stderr,"db_add_archive insert statement prepare failed\n");
+        return false;
+    }
+    results = 0;
+    sqlite3_bind_blob(stmt,1,ent.node_id.uuid,sizeof(ent.node_id.uuid),NULL);/*node_id*/
+    sqlite3_bind_blob(stmt,2,ent.parent_node.uuid,sizeof(ent.parent_node.uuid),NULL);/*parent_node*/
+    sqlite3_bind_text(stmt,3,ent.name.c_str(),-1,NULL);/*name*/
+    sqlite3_bind_blob(stmt,4,&ent.real_name[0],ent.real_name.size(),NULL);/*real_name*/
+    sqlite3_bind_text(stmt,5,ent.name_charset.c_str(),-1,NULL);/*name_charset*/
+    sqlite3_bind_int64(stmt,6,ent.size);/*size*/
+    sqlite3_bind_int(stmt,7,ent.type);/*type*/
+    sqlite3_bind_int64(stmt,8,ent.mtime);/*mtime*/
+    sqlite3_bind_int64(stmt,9,ent.inode);/*inode*/
+    do {
+        sr = sqlite3_step(stmt);
+        if (sr == SQLITE_BUSY) continue;
+        else if (sr == SQLITE_DONE) {
+            results++;
+            break;
+        }
+        else {
+            fprintf(stderr,"SQLITE statement error\n");
+            break;
+        }
+    } while(1);
+    sqlite3_finalize(stmt);
+    if (results == 0) return false;
+
+    return true;
+}
 
--- util/lib_prl_nodes.h
+++ util/lib_prl_nodes.h
@@ -3,8 +3,51 @@
 #define UTIL_LIB_PRL_NODES_H
 
 #include "lib_prluuid.h"
+#include "lib_prl_blob.h"
 
 extern const prluuid prl_zero_node;
+
+enum prl_node_type {
+    NODE_TYPE_NULL=0,                           // 0
+    NODE_TYPE_FILE,                             // 1
+    NODE_TYPE_DIRECTORY,                        // 2
+    NODE_TYPE_ARCHIVE,                          // 3
+    NODE_TYPE_ARCHIVE_COLLECTION,               // 4
+    NODE_TYPE_VIEW,                             // 5
+    NODE_TYPE_SYMLINK,                          // 6
+
+    NODE_TYPE_MAX
+};
+
+enum prl_node_flags {
+    NODE_FLAG_PRIVATE=(1 << 0),                 // private node, do not show publicly
+    NODE_FLAG_USER_MODIFIED_CHARSET=(1 << 1)    // user modified charset, do not auto-modify later
+};
+
+/* instead of passing a million parameters for each part, pass a struct instead.
+ * addition of fields is just add a new struct member and recompile away. */
+struct prl_node_entry {
+    prluuid         node_id;
+    prluuid         parent_node;
+    std::string     name;
+    prl_blob        real_name;
+    std::string     name_charset;
+    uint64_t        size;
+    unsigned int    type;
+    std::string     mime_string;
+    std::string     content_encoding;
+    unsigned int    flags;
+    unsigned int    index;                      // in case of archive files that list the same file multiple times
+    uint64_t        mtime;
+    uint64_t        inode;
+
+    prl_node_entry() : size(0),type(0),flags(0),index(0),mtime(0),inode(0) { }
+    ~prl_node_entry() { }
+};
+
+bool prl_node_db_open(void);
+void prl_node_db_close(void);
+bool prl_node_db_add_fsentbyname(prl_node_entry &ent);
 
 #endif /*UTIL_LIB_PRL_NODES_H*/
 
