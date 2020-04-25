66389e38fc313e32d2c8946d9ffddaedab880b3a
Jonathan Campbell <jonathan@castus.tv>
2020-04-25 00:51:13 -0700

    more

--- util/mount-is-readonly.cpp
+++ util/mount-is-readonly.cpp
@@ -3,24 +3,29 @@
 #include <sys/statvfs.h>
 #include <stdio.h>
 
+#include <string>
+
 using namespace std;
 
-int main(int argc,char **argv) {
+bool prl_fs_readonly(const char *path) {
     struct statvfs st;
 
+    if (statvfs(path,&st) != 0)
+        return false;
+
+    return (st.f_flag & ST_RDONLY) != 0;
+}
+
+bool prl_fs_readonly(const std::string &path) {
+    return prl_fs_readonly(path.c_str());
+}
+
+int main(int argc,char **argv) {
     if (argc < 2) {
         fprintf(stderr,"Specify location\n");
         return 1;
     }
 
-    if (statvfs(argv[1],&st) != 0) {
-        fprintf(stderr,"Cannot stat\n");
-        return 1;
-    }
-
-    if (st.f_flag & ST_RDONLY)
-        return 0;
-
-    return 1;
+    return prl_fs_readonly(argv[1]) ? 0 : 1;
 }
 
