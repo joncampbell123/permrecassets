0acfca9e8ada39fd141744a06dc59d39028cd027
Jonathan Campbell <jonathan@castus.tv>
2020-04-25 02:25:10 -0700

    more

--- util/lib_prl_nodes.cpp
+++ util/lib_prl_nodes.cpp
@@ -56,6 +56,101 @@ void prl_node_db_close(void) {
 }
 
 /* in ent.node_id
+ * out: array of ent.* */
+bool prl_node_db_lookup_children_of_parent(std::vector<prl_node_entry> &rlist,prl_node_entry &pent) {
+    sqlite3_stmt* stmt = NULL;
+    const char* pztail = NULL;
+    int results,sr;
+
+    rlist.clear();
+
+    /*                                                0           1    2         3            4    5    6           7                8     9     10 */
+    if (sqlite3_prepare_v2(prl_node_db_sqlite,"SELECT node_id,name,real_name,name_charset,size,type,mime_string,content_encoding,flags,mtime,inode FROM nodes WHERE parent_node = ? LIMIT 1000;",-1,&stmt,&pztail) != SQLITE_OK) {
+        fprintf(stderr,"db_add_archive statement prepare failed\n");
+        return false;
+    }
+    results = 0;
+    sqlite3_bind_blob(stmt,1,pent.node_id.uuid,sizeof(pent.node_id.uuid),NULL);/*node_id*/
+    do {
+        sr = sqlite3_step(stmt);
+        if (sr == SQLITE_BUSY) continue;
+        else if (sr == SQLITE_DONE) break;
+        else if (sr == SQLITE_ROW) {
+            prl_node_entry chent;
+
+            {
+                /* parent_node */
+                chent.parent_node = pent.node_id;
+                /* node_id */
+                {
+                    chent.node_id = prl_zero_node;
+                    int blobsz = sqlite3_column_bytes(stmt,0);
+                    if (blobsz == sizeof(chent.node_id.uuid)) {
+                        const void *b = sqlite3_column_blob(stmt,0);
+                        if (b != NULL) memcpy(chent.node_id.uuid,b,sizeof(chent.node_id.uuid));
+                    }
+                }
+                /* name */
+                {
+                    const unsigned char *t = sqlite3_column_text(stmt,1);
+                    chent.name = (t != NULL) ? (char*)t : "";
+                }
+                /* real_name */
+                {
+                    int blobsz = sqlite3_column_bytes(stmt,2);
+                    const void *b = sqlite3_column_blob(stmt,2);
+                    if (blobsz > 0 && b != NULL) {
+                        chent.real_name.resize(blobsz);
+                        memcpy(&chent.real_name[0],b,blobsz);
+                    }
+                    else {
+                        chent.real_name.clear();
+                    }
+                }
+                /* name_charset */
+                {
+                    const unsigned char *t = sqlite3_column_text(stmt,3);
+                    chent.name_charset = (t != NULL) ? (char*)t : "";
+                }
+                /* size */
+                chent.size = sqlite3_column_int64(stmt,4);
+                /* type */
+                chent.type = sqlite3_column_int(stmt,5);
+                /* mime_string */
+                {
+                    const unsigned char *t = sqlite3_column_text(stmt,6);
+                    chent.mime_string = (t != NULL) ? (char*)t : "";
+                }
+                /* content_encoding */
+                {
+                    const unsigned char *t = sqlite3_column_text(stmt,7);
+                    chent.mime_string = (t != NULL) ? (char*)t : "";
+                }
+                /* flags */
+                chent.flags = sqlite3_column_int64(stmt,8);
+                /* mtime */
+                chent.mtime = sqlite3_column_int64(stmt,9);
+                /* inode */
+                chent.inode = sqlite3_column_int64(stmt,10);
+            }
+
+            rlist.push_back(chent);
+            results++;
+        }
+        else {
+            fprintf(stderr,"SQLITE statement error\n");
+            return -1;
+        }
+    } while(1);
+    sqlite3_finalize(stmt);
+
+    if (results >= 0)
+        return true;
+
+    return false;
+}
+
+/* in ent.node_id
  * out: ent.* */
 bool prl_node_db_lookup_by_node_id(prl_node_entry &ent) {
     sqlite3_stmt* stmt = NULL;
