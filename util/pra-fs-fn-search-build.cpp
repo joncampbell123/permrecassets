44e0dade9f43ca4d6d6e8e063d16f14e3587ca87
Jonathan Campbell <jonathan@castus.tv>
2020-08-17 13:32:58 -0700

    more

--- util/pra-fs-fn-search-build.cpp
+++ util/pra-fs-fn-search-build.cpp
@@ -12,7 +12,47 @@
 #include "lib_prl_blob.h"
 #include "lib_prl_nodes.h"
 
+#include <algorithm>
+
 using namespace std;
+
+std::vector<std::string> prl_filename2dict(const std::string &spp) {
+    std::vector<std::string> ret;
+    const char *s = spp.c_str();
+    std::string word;
+
+    while (*s != 0) {
+        if (*s == ' ' || *s == ',' || *s == ';' || *s == '\\') {
+            if (!word.empty()) {
+                ret.push_back(word);
+                word.clear();
+            }
+
+            s++;
+        }
+        else if (*s >= 32 || *s < 0) {
+            if (*s == '.' || *s == '-') {
+                if (!word.empty()) {
+                    ret.push_back(word);
+                    word.clear();
+                }
+            }
+            else {
+                word.push_back(*s);
+            }
+
+            s++;
+        }
+    }
+
+    if (!word.empty()) {
+        ret.push_back(word);
+        word.clear();
+    }
+
+    std::sort(ret.begin(),ret.end());
+    return ret;
+}
 
 int main() {
     if (!prl_node_db_open()) {
@@ -20,14 +60,17 @@ int main() {
         return 1;
     }
 
-    /* TEST */
+    /* build dict */
     {
         prl_file_raw_enum en;
         prl_node_entry node;
 
         if (en.begin_enum()) {
             while (en.next(node)) {
+                std::vector<std::string> dict = prl_filename2dict(node.name);
                 printf("%s: %s\n",node.node_id.to_string().c_str(),node.name.c_str());
+                for (const auto &s : dict) printf("  '%s'\n",s.c_str());
+                printf("\n");
             }
             en.end_enum();
         }
