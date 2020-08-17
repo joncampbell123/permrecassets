9b6dcdbe299ae40925f5925737ef06086821e1df
Jonathan Campbell <jonathan@castus.tv>
2020-08-17 13:47:57 -0700

    more

--- util/pra-fs-fn-search-build.cpp
+++ util/pra-fs-fn-search-build.cpp
@@ -17,37 +17,39 @@
 using namespace std;
 
 std::vector<std::string> prl_filename2dict(const std::string &spp) {
+    std::vector<const char *> wbeg;
     std::vector<std::string> ret;
     const char *s = spp.c_str();
-    std::string word;
+    const char *wordbeg = NULL;
 
     while (*s != 0) {
-        if (*s == ' ' || *s == ',' || *s == ';' || *s == '\\') {
-            if (!word.empty()) {
-                ret.push_back(word);
-                word.clear();
-            }
+        if ((*s > 0 && *s < 32) || *s == ' ' || *s == ',' || *s == ';' || *s == '\\' || *s == '.' || *s == '-' || *s == '+' || *s == '*') {
+            if (wordbeg != NULL) {
+                wbeg.push_back(wordbeg);
+                wordbeg = NULL;
 
-            s++;
-        }
-        else if (*s >= 32 || *s < 0) {
-            if (*s == '.' || *s == '-') {
-                if (!word.empty()) {
-                    ret.push_back(word);
-                    word.clear();
+                for (const auto &sb : wbeg) {
+                    if (sb < s)
+                        ret.push_back(std::move(std::string(sb,(size_t)(s - sb))));
                 }
             }
-            else {
-                word.push_back(*s);
-            }
-
-            s++;
         }
+        else if (*s >= 32 || *s < 0) {
+            if (wordbeg == NULL)
+                wordbeg = s;
+        }
+
+        s++;
     }
 
-    if (!word.empty()) {
-        ret.push_back(word);
-        word.clear();
+    if (wordbeg != NULL) {
+        wbeg.push_back(wordbeg);
+        wordbeg = NULL;
+
+        for (const auto &sb : wbeg) {
+            if (sb < s)
+                ret.push_back(std::move(std::string(sb,(size_t)(s - sb))));
+        }
     }
 
     std::sort(ret.begin(),ret.end());
