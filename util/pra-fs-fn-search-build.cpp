6f5e8793ee0a6032b82b71b7397c4f3813111537
Jonathan Campbell <jonathan@castus.tv>
2020-08-20 06:55:35 -0700

    more

--- util/pra-fs-fn-search-build.cpp
+++ util/pra-fs-fn-search-build.cpp
@@ -15,6 +15,8 @@
 
 using namespace std;
 
+bool debug_out = false;
+
 int main() {
     if (!prl_node_db_open()) {
         fprintf(stderr,"Unable to open SQLite3 DB. Use pra-fs-scan-db-init.sh\n");
@@ -25,21 +27,43 @@ int main() {
     {
         prl_file_raw_enum en;
         prl_node_entry node;
+        unsigned long long kw_count = 0;
+        unsigned long long f_count = 0;
+        time_t ref = 0,now;
 
         if (en.begin_enum()) {
             while (en.next(node)) {
+                f_count++;
+
                 prl_node_db_search_delete_by_type_and_node(prl_search_dict_id_filename,node.node_id);
 
                 std::vector<std::string> dict = prl_filename2dict(node.name);
-                printf("%s: %s\n",node.node_id.to_string().c_str(),node.name.c_str());
+
+                if (debug_out)
+                    printf("%s: %s\n",node.node_id.to_string().c_str(),node.name.c_str());
+
                 for (const auto &s : dict) {
                     std::string n = prl_normalizeword(s);
-                    printf("  '%s' => '%s'\n",s.c_str(),n.c_str());
+
+                    if (debug_out)
+                        printf("  '%s' => '%s'\n",s.c_str(),n.c_str());
+
+                    kw_count++;
                 }
-                printf("\n");
+                if (debug_out)
+                    printf("\n");
+
+                now = time(NULL);
+                if (ref != now) {
+                    printf("\x0D" "files=%llu keywords=%llu" "\x1B[K" "\x1B[?7h",f_count,kw_count);
+                    fflush(stdout);
+                    ref = now;
+                }
             }
             en.end_enum();
         }
+
+        printf("\n");
     }
 
     prl_node_db_scan_commit();
